212536e74cc3e4cbb834b0346ce61ce373d7b7b8
alexrodriguezalejo <alexrodriguezalejo@gmail.com>
2023-05-22 18:28:40 -0300

    PieView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import MenuView from "./views/MenuView.vue";
+import PieView from "./views/PieView.vue";
 </script>
 
 <template>
   <MenuView></MenuView>
   <RouterView />
+  <PieView></PieView>
 </template>
 
 <style></style>
---
+++ frontend/src/views/PieView.vue
@@ -0,0 +1,55 @@
+<template>
+  <br>
+  <footer class="footer bg-dark text-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <p>&copy; 2023 Paseo de Mascotas. Todos los derechos reservados.</p>
+        </div>
+        <div class="col-md-6 text-md-right">
+          <ul class="list-inline">
+            <li class="list-inline-item">
+              <a href="">Inicio</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="">Acerca de</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  padding: 20px 0;
+}
+
+.footer p {
+  margin: 0;
+}
+
+.footer ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.footer ul li {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.footer ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.footer ul li a:hover {
+  color: #ccc;
+}
+</style>
