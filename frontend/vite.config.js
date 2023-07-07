1a37c0120b332ecb4b27c7c34457baab0373d33e
Sobrino Julian <sobrinojulian@protonmail.com>
2023-07-07 18:08:53 -0300

    vite-plugin-imagemin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import imageminPlugin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    imageminPlugin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 5 },
+      mozjpeg: { quality: 80 },
+      pngquant: { quality: [0.8, 0.9], speed: 4 },
+      svgo: {}
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
